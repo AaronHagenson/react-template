bbd23fc89b62e19bbd8ea699784aab6e06d974ec
Eric Tweedt <tweeeri@chrobinson.com>
2017-11-07 15:25:29 -0600

    Prettier

--- nightwatch.js
+++ nightwatch.js
@@ -11,8 +11,14 @@ if (!fs.existsSync(nightwatchGlobalsFile)) {
   const endOfLine = require('os').EOL;
   const tab = '\t';
   // create default global
-  const _global = 'module.exports = {' + endOfLine + tab + 'token_hash: null' + endOfLine + '};';
-  fs.writeFileSync(nightwatchGlobalsFile, _global.replace(/\"/g, ""));
+  const _global =
+    'module.exports = {' +
+    endOfLine +
+    tab +
+    'token_hash: null' +
+    endOfLine +
+    '};';
+  fs.writeFileSync(nightwatchGlobalsFile, _global.replace(/\"/g, ''));
 }
 
 // config for tokenization
@@ -39,64 +45,61 @@ const isWindows = () => {
 };
 
 if (isWindows()) {
-  nightwatchConfig.chrome.driver_path = "./bin/chromedriver.exe";
+  nightwatchConfig.chrome.driver_path = './bin/chromedriver.exe';
 } else {
-  nightwatchConfig.chrome.driver_path = "./bin/chromedriver";
+  nightwatchConfig.chrome.driver_path = './bin/chromedriver';
 }
 
 // nightwatch.json (config)
 module.exports = {
-  "src_folders": ["./e2e"],
-  "output_folder": "e2e_results",
-  "globals_path": nightwatchGlobalsFile,
-  "selenium": {
-    "start_process": nightwatchConfig.selenium.start_process,
-    "server_path": nightwatchConfig.selenium.server_path,
-    "log_path": "e2e_results",
-    "host": nightwatchConfig.selenium.host,
-    "port": nightwatchConfig.selenium.port,
-    "cli_args": {
-      "webdriver.chrome.driver": nightwatchConfig.chrome.driver_path
+  src_folders: ['./e2e'],
+  output_folder: 'e2e_results',
+  globals_path: nightwatchGlobalsFile,
+  selenium: {
+    start_process: nightwatchConfig.selenium.start_process,
+    server_path: nightwatchConfig.selenium.server_path,
+    log_path: 'e2e_results',
+    host: nightwatchConfig.selenium.host,
+    port: nightwatchConfig.selenium.port,
+    cli_args: {
+      'webdriver.chrome.driver': nightwatchConfig.chrome.driver_path
     }
   },
-  "test_settings": {
-    "default": {
-      "launch_url": nightwatchConfig.launch_url,
-      "launch_url_auth_hash": nightwatchConfig.launch_url_auth_hash,
-      "selenium_port": nightwatchConfig.selenium.port,
-      "selenium_host": nightwatchConfig.selenium.host,
-      "silent": nightwatchConfig.silent,
-      "sync_test_names": true,
-      "screenshots": {
-        "enabled": false,
-        "path": ""
+  test_settings: {
+    default: {
+      launch_url: nightwatchConfig.launch_url,
+      launch_url_auth_hash: nightwatchConfig.launch_url_auth_hash,
+      selenium_port: nightwatchConfig.selenium.port,
+      selenium_host: nightwatchConfig.selenium.host,
+      silent: nightwatchConfig.silent,
+      sync_test_names: true,
+      screenshots: {
+        enabled: false,
+        path: ''
       },
-      "desiredCapabilities": {
-        "browserName": "chrome"
+      desiredCapabilities: {
+        browserName: 'chrome'
       },
-      "globals": {
-        "waitForConditionTimeout": 60000,
-        "asyncHookTimeout": 60000
+      globals: {
+        waitForConditionTimeout: 60000,
+        asyncHookTimeout: 60000
       }
     },
-    "phantomjs": {
-      "desiredCapabilities": {
-        "browserName": "phantomjs",
-        "javascriptEnabled": true,
-        "acceptSslCerts": true,
-        "phantomjs.binary.path": nightwatchConfig.phantomjs.driver_path
+    phantomjs: {
+      desiredCapabilities: {
+        browserName: 'phantomjs',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        'phantomjs.binary.path': nightwatchConfig.phantomjs.driver_path
       }
     },
-    "chrome": {
-      "desiredCapabilities": {
-        "browserName": "chrome",
-        "javascriptEnabled": true,
-        "acceptSslCerts": true,
-        "chromeOptions": {
-          "args": [
-            "ignore-certificate-errors",
-            "test-type"
-          ]
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['ignore-certificate-errors', 'test-type']
         }
       }
     }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -28,7 +28,8 @@ export default {
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
-    new HtmlWebpackPlugin({     // Create HTML file that includes references to bundled CSS and JS.
+    new HtmlWebpackPlugin({
+      // Create HTML file that includes references to bundled CSS and JS.
       template: 'src/index.ejs',
       minify: {
         removeComments: true,
@@ -45,30 +46,50 @@ export default {
           includePaths: [path.resolve(__dirname, 'src', 'scss')]
         },
         context: '/',
-        postcss: () => [autoprefixer],
+        postcss: () => [autoprefixer]
       }
     }),
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
-    new CopyWebpackPlugin([{
-      from: 'src/config/*',
-      flatten: true
-    }, {
-      from: 'src/health.html'
-    }])
+    new CopyWebpackPlugin([
+      {
+        from: 'src/config/*',
+        flatten: true
+      },
+      {
+        from: 'src/health.html'
+      }
+    ])
   ],
   module: {
     rules: [
       {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
       {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
-      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
-      {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'},
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      },
       {test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]'},
       {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'},
-      {test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']}
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        loaders: [
+          'style-loader',
+          'css-loader?sourceMap',
+          'postcss-loader',
+          'sass-loader?sourceMap'
+        ]
+      }
     ]
   }
 };
